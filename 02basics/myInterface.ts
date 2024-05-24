d178d0dda3bca9ae7b7d1a8e620aa20c3cb1e6db
Bihan Banerjee <bihanbanerjee@Bihans-MacBook-Pro.local>
2024-05-24 23:38:39 +0530

    Add comments -> files in 02basics

--- 02basics/myInterface.ts
+++ 02basics/myInterface.ts
@@ -1,30 +1,35 @@
 interface User {
-    readonly dbId: number
-    email: string,
-    userId: number,
-    googleId?: string
-    // startTrail: () => string
-    startTrail(): string
-    getCoupon(couponname: string, value: number): number
+  readonly dbId: number;
+  email: string;
+  userId: number;
+  googleId?: string; // It is an optional attribute. It can or can not be present in the object. Not a mandatory key.
+  // startTrail: () => string
+  startTrail(): string; // The object will have a method which will return a string type value.
+  getCoupon(couponname: string, value: number): number;
 }
 
-interface User {
-    githubToken: string
+interface User2 {
+  githubToken: string;
 }
 
-interface Admin extends User {
-    role: "admin" | "ta" | "learner"
+interface Admin extends User, User2 {
+  role: "admin" | "ta" | "learner";
 }
 
-const hitesh: Admin = { dbId: 22, email: "h@h.com", userId: 2211,
-role: "admin",
-githubToken: "github",
-startTrail: () => {
-    return "trail started"
-},
-getCoupon: (name: "hitesh10", off: 10) => {
-    return 10
-}
-}
-hitesh.email = "h@hc.com"
+const hitesh: Admin = {
+  dbId: 22,
+  email: "h@h.com",
+  userId: 2211,
+  role: "admin",
+  //googleId is an optional key.
+  githubToken: "github",
+  startTrail: () => {
+    return "trail started";
+  },
+  getCoupon: (name: "hitesh10", off: 10) => {
+    return 10;
+  },
+};
+hitesh.email = "h@hc.com";
 // hitesh.dbId = 33
+
--- 02basics/myTupEnums.ts
+++ 02basics/myTupEnums.ts
@@ -5,12 +5,26 @@ tUser = ["hc", 131, true];
 
 let rgb: [number, number, number] = [255, 123, 112];
 
-type User = [number, string];
+type User = [number, string]; // tuples in typescript
 
 const newUser: User = [112, "example@google.com"];
 
-newUser[1] = "hc.com";
+newUser[1] = "hc.com"; //you can do this as the 2nd value is a string type.
 newUser.push(true);
 
-export {};
+/*
+ * Tuple types are a type of array of known length and where the different elements may have different types.
+ * A value of type [number, string] is guaranteed to have a length of 2, with a number at element 0 and a string at element 1.
+ * Why then, does TypeScript allow you to call methods like push(), pop(), shift(), unshift(), and splice() on values of tuple
+ * types, when such methods generally destroy the supposed guarantees of tuple types? Shouldn't it stop you from doing that,
+ * just like it stops you from assigning a value like [1, "two", "three"] to [number, string] in the first place?
+ */
 
+/*Answer to the above question:->
+ * Yeah, it's not great.
+ * I don't know that there's a good canonical answer to this. The closest I can find is microsoft/TypeScript#6325,
+ * which proposes that such methods be omitted from tuple types. This proposal was declined, on the possible grounds
+ * that it would be a breaking change for existing real-world code.
+ */
+
+export {};
